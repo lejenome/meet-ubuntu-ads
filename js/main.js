b8fa55093d41948ba5fa40f582f728ab6f9d2995
Moez Bouhlel <bmoez.j@gmail.com>
2013-11-27 13:42:13 +0100

    improve code style

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-(function(){
-    var imp = impress();
+(function () {
+    "use strict";
+    var imp = impress(),
+        el = document.querySelector('#arrowLeft');
     imp.init();
-    var el = document.querySelector('#arrowLeft');
-    el.addEventListener('click', function(e){
+    if (el) {
+        el.addEventListener('click', function (e) {
             imp.prev();
             e.preventDefault();
-
-    },false);
-    el = document.querySelector('#arrowRight');
-    el.addEventListener('click', function(e){
-        imp.next();
-        e.preventDefault();
-    },false);
+        }, false);
+        el = document.querySelector('#arrowRight');
+        el.addEventListener('click', function (e) {
+            imp.next();
+            e.preventDefault();
+        }, false);
+    }
 })();
